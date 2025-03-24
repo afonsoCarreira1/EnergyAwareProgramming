152aeed2e7641bb9fc5f4c24b557f229705b0a37
afonsoCarreira1 <afonso1carreira@gmail.com>
2025-03-24 18:42:20 +0000

    started using maven

---
+++ codegen/src/main/java/com/template/aux/TemplatesAux.java
@@ -0,0 +1,50 @@
+package com.template.aux;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TemplatesAux {
+
+    public static boolean stop = false;
+
+    //public static void writeProgramInfo(int i) {
+    //    WritePid.writeTargetProgInfo("timeStamp", i);
+    //}
+
+    public static void sendStopSignalToOrchestrator(String pid) throws IOException {
+        Runtime.getRuntime().exec(new String[] { "kill", "-USR2", pid });
+    }
+
+    public static void sendStartSignalToOrchestrator(String pid, int iter) throws IOException, InterruptedException {
+        WritePid.writeTargetProgInfo(Long.toString(ProcessHandle.current().pid()), iter);
+        Runtime.getRuntime().exec(new String[] { "kill", "-USR1", pid });
+        Thread.sleep(100);
+    }
+
+    public static void writeErrorInFile(String filename, String errorMessage) {
+        System.out.println("this happened -> "+errorMessage);
+        try {
+            File myObj = new File("errorFiles/" +filename+".txt");
+            myObj.createNewFile();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        try (FileWriter writer = new FileWriter("errorFiles/" + filename+".txt")) {
+            writer.write(errorMessage);
+        } catch (IOException e) {System.out.println(e.getMessage());}
+    }
+
+    public static void launchTimerThread() {
+        Thread timerThread = new Thread(() -> {
+            try {
+                Thread.sleep(1100);
+                stop = true;
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        timerThread.start();
+    }
+
+}

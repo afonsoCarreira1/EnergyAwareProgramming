563981269c72b451ed31cd6315612f479305b534
afonsoCarreira1 <afonso1carreira@gmail.com>
2025-04-24 19:14:22 +0100

    updated batch script

--- batch_script_codegen.sh
+++ batch_script_codegen.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 #SBATCH --job-name=codegen
 #SBATCH --output=log_codegen_%A_%a.log
-#SBATCH --array=0-0
+#SBATCH --array=0-9
 #SBATCH --ntasks=1
 #SBATCH --mem=1G
 
 #test args
 ARGS=(
     "lists addAll",
-    "lists containsAll"
+    "lists containsAll",
+    "lists add",
+    "lists get",
+    "lists equals",
+    "lists replaceAll",
+    "lists retainAll",
+    "lists set",
+    "lists subList",
+    "lists size"
 ) 
 
 ./run_codegen.sh ${ARGS[$SLURM_ARRAY_TASK_ID]}

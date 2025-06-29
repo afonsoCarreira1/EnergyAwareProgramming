4295e485d064f98f05d809c373167950fd49f493
afonsoCarreira1 <afonso1carreira@gmail.com>
2025-06-29 01:33:06 +0100

    update rel

--- parser/src/main/java/com/parse/App.java
+++ parser/src/main/java/com/parse/App.java
@@ -8,11 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        ASTFeatureExtractor parser = new ASTFeatureExtractor("src/main/java/com/parse/","TestFile",false,false);
-        HashMap<String, Map<String, Object>> methods = parser.getFeatures();
-        for (String key : methods.keySet()) {
-            System.out.println("key -> "+key);
-        }
+        //ASTFeatureExtractor parser = new ASTFeatureExtractor("src/main/java/com/parse/","TestFile",false,false);
+        //HashMap<String, Map<String, Object>> methods = parser.getFeatures();
+        //for (String key : methods.keySet()) {
+        //    System.out.println("key -> "+key);
+        //}
         //key -> TestObject.yes(java.lang.Boolean | java.lang.String)
         //key -> TestObject.yes(Boolean | String)
         //Map<String, Object> methodfeatures = methods.get("TestModelProg.fun(ArrayList | ArrayList)");

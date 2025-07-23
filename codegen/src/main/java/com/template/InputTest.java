4c5238fd497196a4a34ee45e3cffaab731900ff3
afonsoCarreira1 <afonso1carreira@gmail.com>
2025-07-23 13:50:25 +0100

    update rel

--- codegen/src/main/java/com/template/InputTest.java
+++ codegen/src/main/java/com/template/InputTest.java
@@ -26,7 +26,7 @@ import spoon.reflect.factory.Factory;
 public class InputTest {
 
     int timeoutMilliseconds = 10_000;//10 seconds
-    int maxInputToTest = 100_000;
+    int maxInputToTest = 1_000_000_000;
     String filename;
     String program;
     String template;
